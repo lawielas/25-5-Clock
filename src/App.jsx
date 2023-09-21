bd5967867729579faff1a077390011e3b9d0d48a
lawielas <lawie.aminu@yahoo.com>
2023-09-21 17:03:11 +0100

    Project setup with react + vite + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>25 + 5 Clock</h1>
+      <div>
+        <div>
+          <h1 id='break-label'>Break Length</h1>
+        </div>
+        <div>
+          <h1 id='session-label'>Session Length</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
